af7322208290430fd5ee0f8fe870754d86c1d040
hidelgionovela <89780156+hidelgionovela@users.noreply.github.com>
2023-08-04 21:13:24 +0200

    feat: create posts

--- app/app/Http/Controllers/Api/PostsController.php
+++ app/app/Http/Controllers/Api/PostsController.php
@@ -3,9 +3,83 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PostsController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $post = new Post;
+        $post->user_id = Auth::user()->id;
+        $post->description = $request->description;
+
+        // Verificando se o campo photo foi preenchido ou nao
+        if ($request->photo != '') {
+            # code...
+            $photo = time().'jpg';
+            file_put_contents('storage/posts/'.$photo,base64_decode($request->photo));
+            $post->photo = $photo;
+        }
+
+        $post->save();
+        $post->user;
+        return response()->json([
+            'success' => true,
+            'message' => 'posted',
+            'post' => $post
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
 }
