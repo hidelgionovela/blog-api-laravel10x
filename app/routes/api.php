fd095d0a9c80dc4a8538b9c7fc2986724ad8b990
hidelgionovela <89780156+hidelgionovela@users.noreply.github.com>
2023-08-04 21:13:59 +0200

    chore: add post routes

--- app/routes/api.php
+++ app/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\PostsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,9 +21,17 @@ use App\Http\Controllers\Api\AuthController;
 // });
 
 
+// User Routes
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::get('/logout', [AuthController::class, 'logout']);
+
+
+// Post Routes
+Route::post('posts/create', [PostsController::class, 'store']);
+Route::post('posts/delete', [PostsController::class, 'destroy']);
+Route::post('posts/update', [PostsController::class, 'update']);
+Route::get('posts', [PostsController::class, 'index']);
 
 
 
