38470a2e2163f54ef10cfb03e1772cb868b7fb7c
hidelgionovela <89780156+hidelgionovela@users.noreply.github.com>
2023-08-05 23:27:34 +0200

    feat: comments in posts

--- app/app/Http/Controllers/Api/CommentsController.php
+++ app/app/Http/Controllers/Api/CommentsController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Comment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CommentsController extends Controller
 {
@@ -28,7 +30,18 @@ class CommentsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $comment = new Comment;
+        $comment->user_id = Auth::user()->id;
+        $comment->post_id = $request->id;
+        $comment->comment = $request->comment;
+
+        $comment->save();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'comment added'
+            // 'post' => $comment
+        ]);
     }
 
     /**
@@ -37,6 +50,23 @@ class CommentsController extends Controller
     public function show(string $id)
     {
         //
+    }
+
+
+    public function comment(Request $request)
+    {
+        $comments = Comment::where('post_id', $request->id)->get();
+
+        foreach ($comments as $comment) {
+            # get user of post
+            $comment->user;
+           
+        }
+
+        return response()->json([
+            'success'=>true,
+            'comments' => $comments
+        ]);
     }
 
     /**
@@ -50,16 +80,49 @@ class CommentsController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request)
     {
-        //
+        $comment = Comment::find($request->id);
+
+        // Verificando se ee o user que esta a editar seu comentario
+        if (Auth::user()->id != $request->id) {
+            # code...
+            return response()->json([
+                'success' => false,
+                'message' => 'unauthorized access'
+            ]);
+        }
+        $comment->comment = $request->comment;
+        $comment->update();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'comment edited'
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Request $request)
     {
-        //
+        $comment = Comment::find($request->id);
+
+        // Verificando se ee o user que esta a editar seu comentario
+        if (Auth::user()->id != $comment->id) {
+            # code...
+            return response()->json([
+                'success' => false,
+                'message' => 'unauthorized access'
+            ]);
+        }
+
+        $comment->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'post deleted'
+        ]);
+
     }
 }
