adac67bae38675c649870a2ed77f3d9088b911ee
hidelgionovela <89780156+hidelgionovela@users.noreply.github.com>
2023-08-05 23:27:53 +0200

    feat: likes in posts

--- app/app/Http/Controllers/Api/LikesController.php
+++ app/app/Http/Controllers/Api/LikesController.php
@@ -3,63 +3,32 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Like;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LikesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function like(Request $request)
     {
-        //
-    }
+        $like = Like::where('post_id', $request->id)->where('user_id',Auth::user()->id)->get();
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+        if (count($like)>0) {
+            $like[0]->delete();
+            return response()->json([
+                'success'=>true,
+                'message' => 'unliked'
+            ]);
+        }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+        $like = new Like;
+        $like->user_id = Auth::user()->id;
+        $like->post_id = $request->id;
+        $like->save();
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return response()->json([
+            'success'=>true,
+            'comments' => 'liked'
+        ]);
     }
 }
